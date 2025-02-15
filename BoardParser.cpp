818965b87bc7b8a5a69b9c71b0023d106bc297e9
printertanida <printer.rung@gmail.com>
2025-02-16 00:47:17 +0700

    Built color piece

--- BoardParser.cpp
+++ BoardParser.cpp
@@ -34,72 +34,39 @@ bool BoardParser::ParseMoved(char c)
 
 BoardParser::BoardParser(Board& board) : m_board(board)
 {
-	LoadColorMap("color.txt");
-}
-
-void BoardParser::LoadColorMap(const std::string& path)
-{
-	std::ifstream file(path);
-	std::string line;
-	while (std::getline(file, line)) {
-		std::stringstream ss(line);
-		std::string pieceChar, colorStr;
-		char delimiter;
-
-		std::getline(ss, pieceChar, ',');
-		std::getline(ss, colorStr);
-
-		PieceType type = ParseType(pieceChar[0]);
-		PieceColor color = ParseColorFromString(colorStr);
-		colorMap[type] = color;
-	}
-}
-
-PieceColor BoardParser::ParseColorFromString(const std::string& colorStr) {
-	if (colorStr == "White") {
-		return PieceColor::white;
-	}
-	else if (colorStr == "Black") {
-		return PieceColor::black;
-	}
-	else {
-		return PieceColor::defult;
-	}
-}
-
-void BoardParser::ParseFile(map<Vector2i, shared_ptr<Piece>>& board,string path)
-{
-
-	cout << "parsing\n";
-	ifstream f(path);
-	string l = "";
-
-	int width, height;
-	Vector2i pos;
-
-	getline(f, l);
-	width = stoi(l);
-	getline(f, l);
-	height = stoi(l);
 	
-	for (int y = height; y > 0; y--)
-	{
-		getline(f, l);
-		cout << l << endl;
-		for (int x = 1; x < width+1; x++)
-		{
-			pos = Vector2i(x, y);
-			char c = toupper(l.at(x - 1));
-			if (c == '.') continue;
-
-			PieceType type = ParseType(c);
-			bool moved = ParseMoved(c);
-			PieceColor color = colorMap.count(type) ? colorMap[type] : PieceColor::defult;
-
-			board[pos] = make_shared<Piece>(pos, c, ParseColor(c));
-
-		}
-	}
-	cout << "parse Completed!\n";
 }
 
+void BoardParser::ParseFile(map<Vector2i, shared_ptr<Piece>>& board, string path)
+{
+    cout << "parsing\n";
+    ifstream f(path);
+    string line;
+
+    while (getline(f, line))
+    {
+        stringstream ss(line);
+        string typeStr, colorStr, xStr, yStr;
+
+        getline(ss, typeStr, ',');
+        getline(ss, colorStr, ',');
+        getline(ss, xStr, ',');
+        getline(ss, yStr, ',');
+
+        PieceType type = ParseType(typeStr[0]);
+        PieceColor color = ParseColor(colorStr[0]);
+        int x = stoi(xStr);
+        int y = stoi(yStr);
+
+        Vector2i pos(x, y);
+
+        char pieceChar = toupper(typeStr[0]);
+        if (color == PieceColor::black) {
+            pieceChar = tolower(typeStr[0]);
+        }
+
+        board[pos] = make_shared<Piece>(pos, pieceChar, color);
+    }
+	cout << "parse Completed!\n";
+
+}
